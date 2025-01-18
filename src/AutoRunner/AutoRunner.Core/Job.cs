a5066734f14f02c1bdd0d17efbb00156725e38d4
Matt Wheeler <wheelermt@proton.me>
2025-01-18 20:11:48 +0000

    Add process variable

--- src/AutoRunner/AutoRunner.Core/Job.cs
+++ src/AutoRunner/AutoRunner.Core/Job.cs
@@ -1,10 +1,13 @@
 // Copyright (c) Matt Wheeler
 // Licensed under the MIT License.
 
+using System.Diagnostics;
+
 namespace AutoRunner.Core;
 
 public record Job(string JobName)
 {
+    public Process? Process;
     public bool Finished { get; set; }
     public DateTime? StartTime { get; set; }
     public DateTime? EndTime { get; set; }
