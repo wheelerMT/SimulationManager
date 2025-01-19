09481f069084071bc6da6095085d8db80870a9fe
Matt Wheeler <wheelermt@proton.me>
2025-01-19 15:52:25 +0000

    Add initial JobProcess file

---
+++ src/AutoRunner/AutoRunner.Core/JobProcess.cs
@@ -0,0 +1,47 @@
+// Copyright (c) Matt Wheeler
+// Licensed under the MIT License.
+
+using System.Diagnostics;
+
+namespace AutoRunner.Core;
+
+public class JobProcess : Process
+{
+    private readonly TaskCompletionSource<bool> _taskCompletionSource;
+
+    public JobProcess()
+    {
+        EnableRaisingEvents = true;
+        _taskCompletionSource = new TaskCompletionSource<bool>();
+
+        // Attach to the Exited event
+        Exited += (sender, e) =>
+        {
+            _taskCompletionSource.SetResult(true);
+            Console.WriteLine($"JobProcess: Process {ProcessName} has exited.");
+        };
+    }
+
+    public async Task WaitForExitAsync()
+    {
+        if (!Start()) throw new InvalidOperationException($"JobProcess: Process {ProcessName} failed to start.");
+
+        await _taskCompletionSource.Task;
+    }
+
+    // Factory to create a new JobProcess
+    public static JobProcess StartNew(string jobName, string arguments = "")
+    {
+        var jobProcess = new JobProcess
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = jobName,
+                Arguments = arguments,
+                UseShellExecute = true,
+                CreateNoWindow = true
+            }
+        };
+        return jobProcess;
+    }
+}
